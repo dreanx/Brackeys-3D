3c75212e88fbfaba7bf0a0009966fcff076ef52e
dreanx <pierre.basck@gmail.com>
2023-05-26 10:37:51 +0200

    Level Complete and Load NextLevel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,8 +4,14 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 
 {
-    private bool gameHasEnded = false;
+    public bool gameHasEnded = false;
     public float restartDelay = 2f;
+
+    public GameObject completeLevelUI;
+    public void CompleteLevel ()
+    {
+        completeLevelUI.SetActive(true);
+    }
 
     public void EndGame()
 
